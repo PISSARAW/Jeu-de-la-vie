53d407a61177ebc93d148afe38827cf3187a646f
Ginks <pissabruney@hotmail.com>
2017-04-18 16:49:39 +0200

    Création classe nommée CelluleMorte

---
+++ CelluleMorte.java
@@ -0,0 +1,33 @@
+package game;
+
+/**
+ * @author PISSARAW
+ *
+ */
+public class CelluleMorte implements Cellule {
+
+	/* (non-Javadoc)
+	 * @see game.Cellule#nouvelleGeneration(int)
+	 */
+	public Cellule nouvelleGeneration(int nombreDeVoisins) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see game.Cellule#getAsString()
+	 */
+	public String getAsString() {
+		// TODO Auto-generated method stub
+		return "-";
+	}
+
+	/* (non-Javadoc)
+	 * @see game.Cellule#estVivante()
+	 */
+	public boolean estVivante() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
