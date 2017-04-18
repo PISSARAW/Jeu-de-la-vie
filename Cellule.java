1598411ac005e89b4f635e25c6290faa971edbe0
Ginks <pissabruney@hotmail.com>
2017-04-18 16:40:31 +0200

    Créer une interface nommée cellule

--- Cellule.java
+++ Cellule.java
@@ -1,8 +1,19 @@
-packages game;
+package game;
+
 
 public interface Cellule
 {
-  Cellule nouvelleGeneration(int nombreDeVoisins);
-  String getAsString();
-  boolean estVivante();
+  /**
+ * @param nombreDeVoisins
+ * @return la cellule de génération suivante en fonction du nombre de voisins
+ */
+Cellule nouvelleGeneration(int nombreDeVoisins);
+  /**
+ * @return une chaîne représentant l’état de la cellule
+ */
+String getAsString();
+  /**
+ * @return Si la cellule est actuellement active ou non.
+ */
+boolean estVivante();
 }
